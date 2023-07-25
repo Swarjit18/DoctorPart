3bfd4c0e1875ad1c55f26466e9bf4f8992459c93
Swarjit Sarkar <117969238+Swarjit18@users.noreply.github.com>
2023-07-25 11:45:44 +0600

    Add files via upload

---
+++ Lab_5/showProduct.php
@@ -0,0 +1,37 @@
+<?php  
+require_once 'controller/productInfo.php';
+
+$student = fetchStudent($_GET['id']);
+
+
+    include "nav.php";
+
+
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+<table>
+	<tr>
+		<th>Name</th>
+		<th>Buying Price</th>
+		<th>Selling Price</th>
+		
+	</tr>
+	<tr>
+		<td><a href="showProduct.php?id=<?php echo $product['ID'] ?>"><?php echo $product['Name'] ?></a></td>
+		<td><?php echo $product['buyingprice'] ?></td>
+		<td><?php echo $product['sellingprice'] ?></td>
+		
+	</tr>
+
+</table>
+
+
+</body>
+</html>
